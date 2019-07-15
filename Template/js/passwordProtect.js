e693f199b5a1bacd4c81cf2b87397a859b4c0455
poorna pakala <ppcrao2004@gmail.com>
2019-07-15 15:54:52 -0500

    Redirect on password fails to index html

--- Template/js/passwordProtect.js
+++ Template/js/passwordProtect.js
@@ -1,15 +1,10 @@
-/**
- * Owl Carousel v2.2.1
- * Copyright 2013-2017 David Deutsch
- * Licensed under  ()
- */
 
 function passWord() {
     var testV = 1;
     var pass1 = prompt('Please Enter Your Password',' ');
     while (testV < 3) {
         if (!pass1)
-            history.go(-1);
+            window.location="../index.html";
         if (pass1.toLowerCase() == "letmein") {
             alert('You Got it Right!');
             window.open('energyLake.html');
@@ -20,6 +15,6 @@ function passWord() {
             prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
     }
     if (pass1.toLowerCase()!="password" & testV ==3)
-        history.go(-1);
+        window.location="../index.html";
     return " ";
 }
